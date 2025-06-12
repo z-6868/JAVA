af27a9f23343897df826c3ce5b320f84687c144e
z-6868 <pioneer_ing@outlook.com>
2025-06-12 20:09:10 +0800

    字面量类型/ValueDemo1.class 输入

---
+++ 字面量类型/ValueDemo1.java
@@ -0,0 +1,8 @@
+public class ValueDemo1{
+	public static void main(String[] args){
+		//运用制表符 对其字面量文本
+System.out.println("name"+'\t'+"age");		
+System.out.println("Elon"+'\t'+"52");		
+	}
+	
+}
---
+++ 输入/ScannerDemo1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ScannerDemo1{
+	public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("请输入整数");
+	
+	int i = sc.nextInt();
+	
+	System.out.println(i);
+	}
+}
